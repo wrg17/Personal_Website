eda74ab28241896cbca7a0e37e64ea2e623bf641
William George <wrg@umich.edu>
2026-06-08 20:37:57 -0400

    chore: untrack build artifacts, ignore generated dirs, pin Node 24

--- .svelte-kit/output/server/nodes/4.js
+++
@@ -1,6 +0,0 @@
-import * as module from '../entries/pages/projects/project/__layout.reset.svelte.js';
-
-export { module };
-export const entry = 'pages/projects/project/__layout.reset.svelte-4ba7523f.js';
-export const js = ["pages/projects/project/__layout.reset.svelte-4ba7523f.js","chunks/vendor-469c7b6c.js","chunks/app-0edb0619.js"];
-export const css = ["assets/pages/projects/project/__layout.reset.svelte-e32b3bd2.css","assets/app-dfb6a14f.css"];
